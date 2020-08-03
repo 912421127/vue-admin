4b27cbe85b3fd37526eea46340916117fbfa6df4
陶震 <912421127@qq.com>
2020-08-03 19:07:17 +0800

    构建项目，配置全局样式，路由

---
+++ src/views/Login/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="login">
+        
+    </div>
+</template>
+<script>
+export default {
+    name: 'login',
+    data () {
+        return {
+            
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+#login {
+    height: 100vh;
+    background-color: #344a5f;
+}
+</style>
